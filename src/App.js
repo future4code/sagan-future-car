7dfe0fd52e96f1d206ca93f4dd7edf146c396c1b
Galanzito <41401599+Galanzito@users.noreply.github.com>
2020-03-10 13:35:50 -0300

    Arquivos Iniciais do Projeto

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { create } from 'jss'
+import { MuiThemeProvider, createGenerateClassName, jssPreset } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { AppContainer } from './components/AppContainer'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const generateClassName = createGenerateClassName()
+const jss = create({
+	...jssPreset(),
+	// We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+	insertionPoint: document.getElementById('jss-insertion-point'),
+})
+
+const theme = createMuiTheme()
+
+function App() {
+	return (
+		<JssProvider jss={jss} generateClassName={generateClassName}>
+			<MuiThemeProvider theme={theme}>
+				<Header />
+				<CssBaseline />
+				<AppContainer />
+				<Footer />
+			</MuiThemeProvider>
+			
+		</JssProvider>
+	)
+}
+
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footers = styled.footer`
+
+	
+`
+
+export default function Footer(){
+	return(
+		<Footers>
+			
+		</Footers>
+	)
+}
