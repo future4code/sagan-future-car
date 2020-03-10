5e126fa1226d2b00f1dd9d5b679c0402c6e65210
Galanzito <41401599+Galanzito@users.noreply.github.com>
2020-03-10 15:36:36 -0300

    Header Finalizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import styled from 'styled-components';
+
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
-const Headers = styled.header`
-	background:royalblue;
+const MainHeader = styled.header`
+	background:#143E54;
 	height:10vh;
 	display:flex;
 	justify-content:space-between;
+	padding-right:50px;
 `
 
 const Logo = styled.img`
@@ -16,6 +19,19 @@ const Logo = styled.img`
 	width:8vw;
 	margin-left:10px;
 `
+
+const TituloHeader = styled.h2`
+	color:#FFFFFF;
+	margin-top:2rem;
+`
+
+const temaHeader = createMuiTheme({
+	palette: {
+		primary:{
+			main:'#FFFFFF'
+		}
+	}
+})
 
 export default function Header() {
 	const [anchorEl, setAnchorEl] = React.useState(null);
@@ -28,22 +44,25 @@ export default function Header() {
 		setAnchorEl(null);
 	};
 	return (
-		<Headers>
+		<MainHeader>
 			<Logo src={require('../img/futurecar.png')} alt="logo-da-marca" />
-			<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-				Menu
+			<TituloHeader>FutureCar - Aqui você encontra o seu futuro carro</TituloHeader>
+			<MuiThemeProvider theme={temaHeader}>				
+				<Button color='primary' aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+					Menu
 			</Button>
-			<Menu
-				id="simple-menu"
-				anchorEl={anchorEl}
-				keepMounted
-				open={Boolean(anchorEl)}
-				onClose={handleClose}
-			>
-				<MenuItem onClick={handleClose}>Home</MenuItem>
-				<MenuItem onClick={handleClose}>Comprar</MenuItem>
-				<MenuItem onClick={handleClose}>Vender</MenuItem>
-			</Menu>
-		</Headers>
+				<Menu
+					id="simple-menu"
+					anchorEl={anchorEl}
+					keepMounted
+					open={Boolean(anchorEl)}
+					onClose={handleClose}
+				>
+					<MenuItem onClick={handleClose}>Home</MenuItem>
+					<MenuItem onClick={handleClose}>Comprar Veículo</MenuItem>
+					<MenuItem onClick={handleClose}>Vender Veículo</MenuItem>
+				</Menu>
+			</MuiThemeProvider>
+		</MainHeader>
 	)
 }
