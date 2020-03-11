6058a13729cfc60c2dae50c8b98301deb348d255
Galanzito <41401599+Galanzito@users.noreply.github.com>
2020-03-11 15:41:08 -0300

    Página de Compras - Nua e Crua

---
+++ src/components/BuyACar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+import Footer from './Footer';
+
+
+
+export default class BuyACar extends Component{
+	constructor(props){
+		super(props)
+	}
+
+	render(){
+		return(
+			<div>
+				<Header />
+				<Footer />
+			</div>
+		)
+	}
+}
